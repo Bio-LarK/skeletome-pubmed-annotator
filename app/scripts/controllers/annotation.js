e03e8d084e645aec94d3b358d83ecfab3446a52f
Craig <cmcnamara87@gmail.com>
2014-06-16 15:55:57 +1000

    Add displaying the abstract from a pubmed article to the page

--- app/scripts/controllers/annotation.js
+++ app/scripts/controllers/annotation.js
@@ -14,10 +14,22 @@ angular.module('skeletomePubmedAnnotatorApp')
             'AngularJS',
             'Karma'
         ];
+        $scope.pubmedId = 24891183;
 
         $scope.getPubmed = function (pubmedId) {
             Restangular.one('pubmed', pubmedId).get().then(function (pubmed) {
                 $scope.pubmed = pubmed;
+                var abstractTextArray = pubmed.PubmedArticle.MedlineCitation.Article.Abstract.AbstractText;
+                $scope.abstract = _.reduce(abstractTextArray, function (abstract, text) {
+                    return abstract + text;
+                }, '');
+
+                // Get all annotations
+                console.log('pubmed', pubmed);
+
+                pubmed.all('annotations').getList({}).then(function (annotations) {
+                    $scope.annotations = annotations;
+                });
             });
         };
 
