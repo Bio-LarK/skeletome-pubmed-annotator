a8db7a979cdd3b23126eb3c97736b7d5f1e69379
Craig <cmcnamara87@gmail.com>
2014-08-11 16:39:51 +1000

    Add DOIDs to MeSH page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -88,9 +88,11 @@ angular.module('skeletomePubmedAnnotatorApp', [
         $rootScope.searchbar = searchbar;
 
     })
-    .config(function ($stateProvider, $urlRouterProvider) {
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
         // For any unmatched url, redirect to /state1
         $urlRouterProvider.otherwise('/');
+
+        // $locationProvider.html5Mode(true);
         //
         // Now set up the states
         $stateProvider
